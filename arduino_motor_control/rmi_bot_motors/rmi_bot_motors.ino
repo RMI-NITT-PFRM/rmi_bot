c146e2f5259d65d0a32aea1f3679304388d320fb
RationalAsh <ashwinnarayan1994@gmail.com>
2014-03-06 13:03:17 +0530

    Updated the arduino code. @Gibin! This is the latest version we have :)

--- arduino_motor_control/rmi_bot_motors/rmi_bot_motors.ino
+++ arduino_motor_control/rmi_bot_motors/rmi_bot_motors.ino
@@ -12,12 +12,19 @@
     installed.
     
     After uploading this code to the Arduino run 
+
     rosrun rosserial_python serial_node.py /dev/ttyACM0
+
     to connect to the arduino.
+	
+	*Update*
+	1. Code has been updated with interrupts to give the encoder reading.
+	2. Does not respond to power levels on rmotor and lmotor topics yet.
+	   but the code does contain functions for making the robot move back
+	   forward, left and right.
 */
 
 #include <ros.h>
-#include <std_msgs/String.h>
 #include <std_msgs/Int16.h>
 
 ros::NodeHandle nh;
@@ -27,21 +34,56 @@ std_msgs::Int16 rightEncoder;
 ros::Publisher lwheel("lwheel", &leftEncoder);
 ros::Publisher rwheel("rwheel", &rightEncoder);
 
+//Lefmomst wire is wire1 (For the motor driver)
+//Wire 1 goes to 13
+//wire 2 goes to 12
+//Wire 3 goes to 11
+//Wire 4 goes to 10
+
+void goBack()
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+}
+
+void goForward()
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+}
+
+void turnRight()
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(10, HIGH);
+}
+
+void turnLeft()
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, LOW);
+}
+
+
 int i = 10030;
 int flag = 1;
 
 void lmotorCB(const std_msgs::Int16& lmotorPower)
 {
-  //if(toggle_msg.data == 1) flag ^= 1;
-  //digitalWrite(13, flag);
-  leftEncoder = lmotorPower;
+  //leftEncoder = lmotorPower;
 }
 
 void rmotorCB(const std_msgs::Int16& rmotorPower)
 {
-  //if(toggle_msg.data == 1) flag ^= 1;
-  //digitalWrite(13, flag);
-  rightEncoder = rmotorPower;
+    //rightEncoder = rmotorPower;
 }
 
 ros::Subscriber<std_msgs::Int16> lmotor("lmotor", &lmotorCB);
@@ -55,6 +97,17 @@ void setup()
   nh.subscribe(lmotor);
   nh.subscribe(rmotor);
   pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  attachInterrupt(0,leftEnc, RISING);
+  attachInterrupt(1, rightEnc, RISING);
+  leftEncoder.data = 0;
+  rightEncoder.data = 0;
 }
 
 void loop()
@@ -62,5 +115,16 @@ void loop()
   lwheel.publish(&leftEncoder);
   rwheel.publish(&rightEncoder);
   nh.spinOnce();
+  turnLeft();
   delay(1000);
+}
+
+void leftEnc()
+{
+  leftEncoder.data += 1;
+}
+
+void rightEnc()
+{
+  rightEncoder.data += 1;
 }
